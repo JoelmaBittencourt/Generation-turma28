633e23e22bf2af49d4532c0ebfb2409f5e87af0b
joelma <joohtrader@gmail.com>
2021-07-26 09:23:43 -0300

    exercicios

---
+++ Exercicios/src/Controle/For2.java
@@ -0,0 +1,26 @@
+package Controle;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int cont;
+for(cont=0;cont<=20;cont++) {
+	System.out.println(cont);
+}
+int x;
+for(x=0;x<10;x++) {
+	System.out.println(x);
+}
+int y;
+for(y=2;y<50;y+=2) {
+	System.out.println(y);
+}
+int a;
+for(a=5;a<25;a+=5) {
+	System.out.println(a);
+}
+}
+	}
+
+
---
+++ Exercicios/src/Controle/Repeticao6.java
@@ -0,0 +1,39 @@
+package Controle;
+
+public class Repeticao6 {
+
+	public static void main(String[] args) {
+		int contador = 20;
+		while (contador < 200) {
+			System.out.println(contador);
+			contador += 20;
+		}
+		int cont = 10;
+		while (cont < 100) {
+			System.out.println(cont);
+			cont += 10;
+		}
+		int x = 5;
+		while (x < 50) {
+			System.out.println(x);
+			x += 5;
+
+		}
+		int y = 2;
+		while (y < 20) {
+			System.out.println(y);
+			y += 2;
+		}
+		int a = 1;
+		while (a < 10) {
+			System.out.println(a);
+			a++;
+		}
+		int b = 1;
+		while (b < 5) {
+			System.out.println("Bom dia");
+			b++;
+		}
+	}
+
+}
