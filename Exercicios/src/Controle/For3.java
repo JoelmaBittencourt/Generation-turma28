6a109a5db7a819088ea87b43a1a218ae81fb5481
joelma <joohtrader@gmail.com>
2021-07-26 16:19:05 -0300

    atividades

---
+++ Exercicios/src/Controle/For3.java
@@ -0,0 +1,15 @@
+package Controle;
+
+public class For3 {
+
+	public static void main(String[] args) {
+		int x;//defina a variavel
+		for(x=5;x<10;x++) {//x=5-onde se inicia a contagem
+			System.out.println("Maria");//x<10 contar ate 9
+			//x++ =x+1
+			
+		}
+
+	}
+
+}
