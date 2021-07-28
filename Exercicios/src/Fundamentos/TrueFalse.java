23d4da0ade747059e46d8cb1ecb1f847c22209c0
joelma <joohtrader@gmail.com>
2021-07-28 11:08:06 -0300

    novos

---
+++ Exercicios/src/Fundamentos/TrueFalse.java
@@ -0,0 +1,15 @@
+package Fundamentos;
+
+public class TrueFalse {
+
+	public static void main(String[] args) {
+		int n1 = 2, n2 = 4, n3 = 7;
+		System.out.printf("n1 é maior que n2? ");
+		System.out.println(n3 > n1);
+		System.out.printf("n2 é menor que n3? ");
+		System.out.println(n2 < n3);
+		System.out.printf("n1 é maior que n2 ");
+		System.out.println( n1 > n2);
+	}
+
+}
